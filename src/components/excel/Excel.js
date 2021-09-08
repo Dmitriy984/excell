e85591da15cb8b91cc34c1a5c4c27345d04097c2
Dmitriy Mironov <dmitrymironov84@gmail.com>
2021-09-08 14:35:47 +0300

    Framework - Created class Dom

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,18 +1,18 @@
+import {$} from '@core/dom'
+
 export class Excel {
   constructor(selector, options) {
-    this.$el = document.querySelector(selector);
-    this.components = options.components || [];
+    this.$el = $(selector)
+    this.components = options.components || []
   }
 
   getRoot() {
-    const $root = document.createElement('div')
-    $root.classList.add('excel')
+    const $root = $.create('div', 'excel')
 
     this.components.forEach(Component => {
-      const $el = document.createElement('div')
-      $el.classList.add(Component.className)
+      const $el = $.create('div', Component.className)
       const component = new Component($el)
-      $el.innerHTML = component.toHTML()
+      $el.html(component.toHTML())
       $root.append($el)
     })
 
@@ -20,6 +20,6 @@ export class Excel {
   }
 
   render() {
-    this.$el.append(this.getRoot());
+    this.$el.append(this.getRoot())
   }
 }
